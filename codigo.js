d29128e73def7e95b62bd21cbf280cf65b886bb9
FELIPE MANUEL ANDRACA <48803376@ort>
2026-03-18 08:27:30 -0300

    EJ5,6,7

---
+++ codigo.js
@@ -0,0 +1,28 @@
+//-------------------EJ05-----------------------//
+
+const Prueba = [2,4,3,5]
+
+const sumarArrays = unArray => {
+    let total = 0;
+    unArray.forEach(element => {
+        total += element;
+    });
+    return total;
+}
+console.log(sumarArrays(Prueba));
+
+//-------------------EJ06-----------------------//
+
+const mayorNum = unArray =>{
+const arrayOrdenado = unArray.sort((a, b) => b - a);
+return unArray[0]
+}
+console.log(mayorNum(Prueba))
+
+//-------------------EJ07-----------------------//
+
+const obtenerPares = unArray =>{
+const pares = unArray.filter(e =>e % 2 === 0);
+return pares;
+}
+console.log(obtenerPares(Prueba));
